a99680fd3af38eb30636cfa7b89bc76828591c3c
maxi75 <63976713+maxi75@users.noreply.github.com>
2025-09-25 20:52:33 +0200

    not working commit

---
+++ src/main/java/de/hausknecht/master/frameworksanddrivers/ui/NodeContainer.java
@@ -0,0 +1,9 @@
+package de.hausknecht.master.frameworksanddrivers.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NodeContainer {
+}
